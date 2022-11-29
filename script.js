ec1b95c7f189e97b8fa17dc711630876045708b2
Jess Peters <jlpeters94@gmail.com>
2022-11-29 10:03:48 +0800

    completed js and added style

--- script.js
+++ script.js
@@ -1,50 +1,63 @@
 const options = document.querySelectorAll("#option");
-
-let pickArray = ["rock", "paper", "scissors"]
-
-function getComputerChoice() {
-	let randomNumber = Math.floor(Math.random()*pickArray.length);
-	let numberToWord = pickArray[randomNumber];
-	return numberToWord;
-}
+const moveHistory = document.createElement("p");
 
 let playerScore = 0;
 let computerScore = 0;
 
-options.forEach((option)) => {
-	option.addEventListener("click", function () {
-		const playerSelection = this.value;
-		const computerSelection = getComputerChoice();
-		game();
+options.forEach((option) => {
+	option.addEventListener("click", () => {
+		const playerSelection = option.value;
+		const cOptions = ["rock", "paper", "scissors"]
+		const computerSelection = cOptions[Math.floor(Math.random() * cOptions.length)];
+		document.querySelector("#winner").textContent = "";
+		game(playerSelection, computerSelection);
 		updateScore();
-	})
+		addHistory(playerSelection, computerSelection);
+		if (checkWinner()) {
+			playerScore = 0;
+			computerScore = 0;
+			updateScore();
+		}
+	});
+	moveHistory.remove();
+});
+
+function addHistory(playerSelection, computerSelection) {
+	moveHistory.innerText = "You chose " +playerSelection.toUpperCase() + ". Computer chose " + computerSelection.toUpperCase() + ".";
+	document.querySelector("#history").appendChild(moveHistory);
 }
 
-function game() {
+function game(playerSelection, computerSelection) {
 
 		if (playerSelection === "rock" && computerSelection === "scissors" || 
 		playerSelection === "paper" && computerSelection === "rock" || 
 		playerSelection === "scissors" && computerSelection === "paper") {
 		playerScore++;
-			alert("You: " + playerSelection + " | Computer: " + computerSelection);
-			alert("You: " + playerScore + " | Computer: " + computerScore);
 		}
 
 		else if (computerSelection === "rock" && playerSelection === "scissors" || 
 		computerSelection === "paper" && playerSelection === "rock" || 
 		computerSelection === "scissors" && playerSelection === "paper") {
 		computerScore++;
-			alert("You: " + playerSelection + " | Computer: " + computerSelection);
-			alert("You: " + playerScore + " | Computer: " + computerScore);
 		}
 
 		else if (playerSelection === computerSelection) {
-			alert("You: " + playerSelection + " | Computer: " + computerSelection);
-			alert("You: " + playerScore + " | Computer: " + computerScore);
 		}
 }
 
 function updateScore() {
-	document.getElementById("#playerScore").textContent = playerScore;
-	document.getElementById("#computerScore").textContent = computerScore;
+	document.querySelector("#playerScore").innerHTML = playerScore;
+	document.querySelector("#computerScore").innerHTML = computerScore;
+}
+
+function checkWinner() {
+	if (playerScore === 5 || computerScore === 5) {
+		const winner =
+		playerScore === 5
+			? "YOU WIN!"
+			: "YOU LOSE!";
+		document.querySelector("#winner").textContent = winner;
+		return true;
+	}
+	return false;
 }
